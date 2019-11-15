d00900bc5f334559a1e13759c4b0970a092e71c2
Jazz Bozner <jazzbozner@gmail.com>
2019-11-15 23:43:57 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,11 +1,13 @@
 def join_ingredients(src)
-  pizza_array=[]
+  array=[]
   counter = 0
-  while counter < src.length
-    pizza_array.push("I love #{src[counter][0]} and #{src[counter][1]} on my pizza")
+  while counter < src.length do
+    array.push("I love #{src[counter][0]} and #{src[counter][1]} on my pizza")
     counter +=1
   end
-  pizza_array
+  array
+end
+
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
@@ -13,32 +15,35 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-end
+
 
 def find_greater_pair(src)
   array=[]
   counter = 0
-  while counter < src.length
+  while counter < src.length do
     array.push(src[counter].max)
     counter +=1
   end
   array
+end
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-end
 
 def total_even_pairs(src)
-total = 0
+  total = 0
   counter = 0
-  while counter < src.length
-  if src[counter][0] % 2 == 0 && src[counter][1] % 2 == 0
-    total = total + (src[counter].sum)
-    counter += 1
-  else
+  while counter < src.length do
+    if (src[counter][0] % 2 == 0) && (src[counter][1] % 2 == 0)
+      total += (src[counter][0] + src[counter][1])
+    end
     counter += 1
   end
   total
+end
+
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
@@ -46,5 +51,4 @@ total = 0
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-end
 
