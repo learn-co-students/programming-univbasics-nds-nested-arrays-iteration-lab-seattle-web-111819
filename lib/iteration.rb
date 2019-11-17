2bc6f3e86e01f92ccd80b2e34fb9b0203f292ae1
Allen Zhang <azhang9328@gmail.com>
2019-11-17 06:36:32 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,12 +6,27 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  newArray = []
+  row_index = 0 
+  while row_index < src.length do
+    newArray.push("I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza")
+    row_index += 1 
+  end 
+  return newArray
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  newArray = []
+  row_index = 0 
+  while row_index < src.length do
+    src[row_index].sort!
+    newArray.push(src[row_index][-1])
+    row_index += 1 
+  end 
+  return newArray
 end
 
 def total_even_pairs(src)
@@ -22,4 +37,15 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  totalEven = 0
+  row_index = 0 
+  while row_index < src.length do 
+    holderN = 0
+    if src[row_index][0].even? && src[row_index][1].even?
+      holderN = src[row_index][0] + src[row_index][1]
+      totalEven = holderN + totalEven
+    end 
+    row_index += 1
+  end 
+  return totalEven
 end
